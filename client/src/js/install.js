bba903e67a3db2622a65a0fc3ec56830e7bf2fbe
Owen M <drawowen77@gmai.com>
2023-08-29 14:26:17 -0700

    finish install and wepack config

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,28 @@
+
 const butInstall = document.getElementById('buttonInstall');
+let defPrompt;
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+    defPrompt = event;
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (defPrompt) {
+        defPrompt.prompt();
+        defPrompt.userChoice.then((choiceRes) => {
+            if (choiceRes.outcome === 'accepted') {
+                butInstall.setAttribute('disabled', true);
+                butInstall.textContent = 'Successfully Installed';
+            } else {
+                console.log('Did not install')
+            }
+        })
+    }
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    butInstall.textContent = 'App Installed';
+    console.log('App Installed', event);
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,12 +16,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.',
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        chunks: ['install'],
+        filename: 'install.html',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'J.A.T.E.',
+        fingerprints: false,
+        inject: true,
+        description: 'PWA Text Editor',
+        background_color: '#000000',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
